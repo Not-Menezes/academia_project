ad0da48c3b7c87ee74117d6c65aecda4331f3fda
Newton <newton.coelho@bigdatacorp.com.br>
2020-12-02 20:27:19 -0300

    adição da tabela de Sessões

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from account.models import Class , Registration
+from account.models import Class , Registration , LoggedInUser
 
 admin.site.register(Class)
 admin.site.register(Registration)
+admin.site.register(LoggedInUser)
